4977d3ed29ec08c6a51419dfc084f43602b62d48
NieceyX <65681516+NieceyX@users.noreply.github.com>
2020-09-26 18:37:15 -0500

    GameStuff

---
+++ Scripts/CameraSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public List<Camera> Cameras;
+    public AudioSource startNoise;
+    private void Start()
+    {
+        EnableCamera(0);
+        startNoise = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EnableCamera(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            startNoise.Play();
+            EnableCamera(1);
+        }
+    }
+
+    private void EnableCamera(int n)
+    {
+        Cameras.ForEach(cam => cam.enabled = false);
+        Cameras[n].enabled = true;
+    }
+}
