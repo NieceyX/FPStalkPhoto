f1ff5038d967dae2be57be492bd234c7055fb5c6
NieceyX <65681516+NieceyX@users.noreply.github.com>
2020-09-26 21:14:07 -0500

    Stuff

---
+++ StalkPhoto/Assets/Scripts/IsVisible.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsVisible : MonoBehaviour
+{
+    Renderer m_Renderer;
+    public bool seen = false;
+    public bool near = false;
+
+    public float radius = 10f;
+    
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    
+    void Update()
+    {
+        if (m_Renderer.isVisible)
+        {
+            //Debug.Log("Object is visible");
+            seen = true;
+        }
+        else
+        {
+            seen = false;
+           // Debug.Log("Object is no longer visible");
+        }
+        Vector3 center = this.gameObject.transform.position;
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].tag == "Player")
+            {
+                near = true;
+                //Debug.Log("close");
+            }
+            else
+            {
+                near = false;
+            }
+            i++;
+        }
+    }
+}
---
+++ StalkPhoto/Assets/Scripts/SoundControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    public AudioSource walk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walk = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            if (!walk.isPlaying)
+            {
+                walk.Play();
+            }
+        }
+
+        else if (Input.GetButton("Horizontal"))
+        {
+            if (!walk.isPlaying)
+            {
+                walk.Play();
+            }
+        }
+        else
+        {
+            walk.Stop();
+        }
+    }
+}
